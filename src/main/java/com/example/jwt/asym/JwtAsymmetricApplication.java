c2cac0f04eb7dd9bb74388ce754a275aefbc70cc
EknarongAphiphutthikul <akenarongake@hotmail.com>
2021-03-17 17:19:00 +0700

    init project

---
+++ src/main/java/com/example/jwt/asym/JwtAsymmetricApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwt.asym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAsymmetricApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAsymmetricApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jwt/asym/JwtAsymmetricApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwt.asym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAsymmetricApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
